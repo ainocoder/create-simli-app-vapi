1a847d9e050eb4e6fed53f0a1734420eca5d12e2
luckoo8 <ainocoder@gmail.com>
2025-05-07 19:06:26 +0900

    return

--- app/SimliVapi.tsx
+++ app/SimliVapi.tsx
@@ -1,14 +1,17 @@
-import React, { use, useCallback, useRef, useState } from "react";
+import React, { use, useCallback, useEffect, useRef, useState } from "react";
 import Vapi from "@vapi-ai/web";
 import { SimliClient } from "simli-client";
 import VideoBox from "./Components/VideoBox";
+import cn from "./utils/TailwindMergeAndClsx";
+import IconSparkleLoader from "@/media/IconSparkleLoader";
 
 interface SimliVapiProps {
   simli_faceid: string;
-  agentId: string;
+  agentId: string; // ElevenLabs agent ID
   onStart: () => void;
   onClose: () => void;
   showDottedFace: boolean;
+  autoPlay?: boolean;
 }
 
 const vapi = new Vapi(process.env.NEXT_PUBLIC_VAPI_API_KEY as string);
@@ -20,42 +23,177 @@ const SimliVapi: React.FC<SimliVapiProps> = ({
   onStart,
   onClose,
   showDottedFace,
+  autoPlay,
 }) => {
   const [isLoading, setIsLoading] = useState(false);
+  const [isAvatarVisible, setIsAvatarVisible] = useState(false);
+  const [error, setError] = useState("");
+  const doRunOnce = useRef(false);
+
   const videoRef = useRef<HTMLVideoElement>(null);
   const audioRef = useRef<HTMLAudioElement>(null);
 
   const handleStart = useCallback(async () => {
     setIsLoading(true);
+    setError("");
     onStart();
     try {
-      if (videoRef.current && audioRef.current) {
-        simliClient.Initialize({
-          apiKey: process.env.NEXT_PUBLIC_SIMLI_API_KEY,
-          faceID: simli_faceid,
-          handleSilence: false,
-          videoRef: videoRef,
-          audioRef: audioRef,
-        } as any);
-      }
+      initializeSimliClient();
       await navigator.mediaDevices.getUserMedia({ audio: true });
       await simliClient?.start();
+      eventListenerSimli();
     } catch (error: any) {
+      setError(`Error starting interaction: ${error.message}`);
       setIsLoading(false);
     }
-  }, [agentId, onStart, simli_faceid]);
+  }, [agentId, onStart]);
+
+  const handleStop = useCallback(() => {
+    setIsLoading(false);
+    setError("");
+    setIsAvatarVisible(false);
+    simliClient?.close();
+    onClose();
+  }, [onClose]);
+
+  const initializeSimliClient = useCallback(() => {
+    if (videoRef.current && audioRef.current) {
+      const SimliConfig = {
+        apiKey: process.env.NEXT_PUBLIC_SIMLI_API_KEY,
+        faceID: simli_faceid,
+        handleSilence: false,
+        videoRef: videoRef,
+        audioRef: audioRef,
+      };
+      simliClient.Initialize(SimliConfig as any);
+    }
+  }, [simli_faceid]);
+
+  const startVapiInteraction = async () => {
+    try {
+      await vapi.start(agentId);
+      eventListenerVapi();
+    } catch (error: any) {
+      setError(`Error starting Vapi interaction: ${error.message}`);
+    }
+  };
+
+  const muteVapiInternalAudio = () => {
+    const audioElements = document.getElementsByTagName("audio");
+    for (let i = 0; i < audioElements.length; i++) {
+      if (audioElements[i].id !== "simli_audio") {
+        audioElements[i].muted = true;
+      }
+    }
+  };
+
+  const getAudioElementAndSendToSimli = () => {
+    if (simliClient) {
+      muteVapiInternalAudio();
+      try {
+        const dailyCall = vapi.getDailyCallObject();
+        const participants = dailyCall?.participants();
+        if (participants) {
+          Object.values(participants).forEach((participant: any) => {
+            const audioTrack = participant.tracks.audio.track;
+            if (audioTrack) {
+              // This is the audio output track for this participant
+              console.log(`Audio track for ${participant.user_name}:`, audioTrack);
+            }
+            if (participant.user_name === "Vapi Speaker") {
+              simliClient.listenToMediastreamTrack(audioTrack as MediaStreamTrack);
+            }
+          });
+        }
+      } catch (error: any) {}
+    } else {
+      setTimeout(getAudioElementAndSendToSimli, 10);
+    }
+  };
+
+  const eventListenerVapi = useCallback(() => {
+    vapi.on("message", (message: any) => {
+      if (
+        message.type === "speech-update" &&
+        message.status === "started" &&
+        message.role === "user"
+      ) {
+        simliClient.ClearBuffer();
+      }
+    });
+    vapi.on("call-start", () => {
+      setIsAvatarVisible(true);
+      getAudioElementAndSendToSimli();
+    });
+    vapi.on("call-end", () => {
+      setIsAvatarVisible(false);
+    });
+  }, []);
+
+  const eventListenerSimli = useCallback(() => {
+    if (simliClient) {
+      simliClient?.on("connected", () => {
+        const audioData = new Uint8Array(6000).fill(0);
+        simliClient?.sendAudioData(audioData);
+        startVapiInteraction();
+      });
+      simliClient?.on("disconnected", () => {
+        vapi.stop();
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    if (autoPlay) {
+      handleStart();
+    }
+  }, [autoPlay]);
 
   return (
-    <div className="flex flex-col items-center justify-center">
-      <VideoBox video={videoRef} audio={audioRef} />
-      <button
-        onClick={handleStart}
-        disabled={isLoading}
-        className="w-full h-[52px] mt-4 bg-simliblue text-white py-3 px-6 rounded-[100px] flex justify-center items-center"
+    <>
+      <div
+        className={`transition-all duration-300 ${
+          showDottedFace ? "h-0 overflow-hidden" : "h-auto"
+        }`}
       >
-        Test Interaction
-      </button>
-    </div>
+        <VideoBox video={videoRef} audio={audioRef} />
+      </div>
+      <div className="flex flex-col items-center">
+        {!isAvatarVisible ? (
+          <button
+            onClick={handleStart}
+            disabled={isLoading}
+            className={cn(
+              "w-full h-[52px] mt-4 disabled:bg-[#343434] disabled:text-white disabled:hover:rounded-[100px] bg-simliblue text-white py-3 px-6 rounded-[100px] transition-all duration-300 hover:text-black hover:bg-white hover:rounded-sm",
+              "flex justify-center items-center"
+            )}
+          >
+            {isLoading ? (
+              <IconSparkleLoader className="h-[20px] animate-loader" />
+            ) : (
+              <span className="font-abc-repro-mono font-bold w-[164px]">
+                Test Interaction
+              </span>
+            )}
+          </button>
+        ) : (
+          <>
+            <div className="flex items-center gap-4 w-full">
+              <button
+                onClick={handleStop}
+                className={cn(
+                  "mt-4 group text-white flex-grow bg-red hover:rounded-sm hover:bg-white h-[52px] px-6 rounded-[100px] transition-all duration-300"
+                )}
+              >
+                <span className="font-abc-repro-mono group-hover:text-black font-bold w-[164px] transition-all duration-300">
+                  Stop Interaction
+                </span>
+              </button>
+            </div>
+          </>
+        )}
+      </div>
+    </>
   );
 };
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,109 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import SimliVapi from "@/app/SimliVapi";
+import DottedFace from "./Components/DottedFace";
+import SimliHeaderLogo from "./Components/Logo";
+import Navbar from "./Components/Navbar";
+import Image from "next/image";
+import GitHubLogo from "@/media/github-mark-white.svg";
 
-const avatar = {
+interface avatarSettings {
+  vapi_agentid: string;
+  simli_faceid: string;
+}
+
+// Customize your avatar here
+const avatar: avatarSettings = {
   vapi_agentid: "26638b9a-a7d0-48c5-9ce8-aba9bb2c1371",
   simli_faceid: "1b8a957b-39cf-4b40-8e84-de676134b892",
 };
 
+interface SimliVapiProps {
+  simli_faceid: string;
+  agentId: string;
+  onStart: () => void;
+  onClose: () => void;
+  showDottedFace: boolean;
+  autoPlay: boolean;
+}
+
 const Demo: React.FC = () => {
   const [showDottedFace, setShowDottedFace] = useState(true);
+  const [agentId, setAgentId] = useState(avatar.vapi_agentid);
+  const [simliFaceId, setSimliFaceId] = useState(avatar.simli_faceid);
+  const [autoPlay, setAutoPlay] = useState(false);
 
-  const onStart = () => setShowDottedFace(false);
-  const onClose = () => setShowDottedFace(true);
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    setAgentId(params.get('agentId') || avatar.vapi_agentid);
+    setSimliFaceId(params.get('faceId') || avatar.simli_faceid);
+    setAutoPlay(params.get('autoplay') === 'true');
+  }, []);
+
+  const onStart = () => {
+    console.log("Setting setshowDottedface to false...");
+    setShowDottedFace(false);
+  };
+
+  const onClose = () => {
+    console.log("Setting setshowDottedface to true...");
+    setShowDottedFace(true);
+  };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-black">
-      <SimliVapi
-        agentId={avatar.vapi_agentid}
-        simli_faceid={avatar.simli_faceid}
-        onStart={onStart}
-        onClose={onClose}
-        showDottedFace={showDottedFace}
-      />
+    <div className="bg-black min-h-screen flex flex-col items-center font-abc-repro font-normal text-sm text-white p-8">
+      <SimliHeaderLogo />
+      <Navbar />
+
+      <div className="absolute top-[32px] right-[32px]">
+        <span
+          onClick={() => {
+            window.open("create-simli-app-vapi-production.up.railway.app");
+          }}
+          className="font-bold cursor-pointer mb-8 text-xl leading-8"
+        >
+          <Image className="w-[20px] inline mr-2" src={GitHubLogo} alt="" />
+          create-simli-app (Vapi)
+        </span>
+      </div>
+      <div className="flex flex-col items-center gap-6 bg-effect15White p-6 pb-[40px] rounded-xl w-full">
+        <div>
+          {showDottedFace && <DottedFace />}
+          <SimliVapi
+            agentId={agentId}
+            simli_faceid={simliFaceId}
+            onStart={onStart}
+            onClose={onClose}
+            showDottedFace={showDottedFace}
+            autoPlay={autoPlay}
+          />
+        </div>
+      </div>
+
+      <div className="max-w-[350px] font-thin flex flex-col items-center ">
+        <span className="font-bold mb-[8px] leading-5 ">
+          {" "}
+          Create Simli App is a starter repo for creating visual avatars with
+          Simli{" "}
+        </span>
+        <ul className="list-decimal list-inside max-w-[350px] ml-[6px] mt-2">
+          <li className="mb-1">
+            Fill in your Vapi and Simli API keys in .env file.
+          </li>
+          <li className="mb-1">
+            Test out the interaction and have a talk with the Vapi-powered,
+            Simli-visualized avatar.
+          </li>
+          <li className="mb-1">
+            You can replace the avatar's face and agent with your own. Do this
+            by editing <code>app/page.tsx</code>.
+          </li>
+        </ul>
+        <span className=" mt-[16px]">
+          You can now deploy this app to Vercel, or incorporate it as part of
+          your existing project.
+        </span>
+      </div>
     </div>
   );
 };
